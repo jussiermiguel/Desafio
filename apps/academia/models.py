6255498da9cf6fe1d845df716d6b37323ea00414
jussiermiguel <jussier.miguel08@gmail.com>
2024-08-24 01:20:44 -0300

    criação dos models

--- apps/academia/models.py
+++ apps/academia/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class AlunoAcademia(models.Model):
+    nome = models.CharField(max_length=255)
+    idade = models.PositiveIntegerField()
+    data_de_nascimento = models.DateField(null=True, blank=True)
+    cpf = models.CharField(max_length=14, unique=True)
+    SEXO_CHOICES = (      
+                  
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    )
+    sexo = models.CharField(max_length=1, choices=SEXO_CHOICES)
+    mensalidade_paga = models.BooleanField(default=True, help_text="Indica se a mensalidade do aluno está paga ou não.")
+   
+    
+    def __str__(self):
+        return self.nome
